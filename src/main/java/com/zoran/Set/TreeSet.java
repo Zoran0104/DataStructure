247e013ff5dcf0a32632931b09d685c073ef976c
Zoran <zwyliufeng@outlook.com>
2021-02-15 13:51:23 +0800

    添加集合代码

---
+++ src/main/java/com/zoran/Set/TreeSet.java
@@ -0,0 +1,47 @@
+package com.zoran.Set;
+
+import com.zoran.tree.BinaryTree;
+import com.zoran.tree.RBTree;
+
+public class TreeSet<E> implements Set<E> {
+    private RBTree<E> tree = new RBTree<>();
+    @Override
+    public int size() {
+        return tree.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return tree.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+        tree.clear();
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return tree.contains(element);
+    }
+
+    @Override
+    public void add(E element) {
+        tree.add(element);
+    }
+
+    @Override
+    public void remove(E element) {
+        tree.remove(element);
+    }
+
+    @Override
+    public void traversal(Visitor<E> visitor) {
+        tree.onOrderTraversal(new BinaryTree.Operation<E>() {
+            @Override
+            public boolean operation(E element) {
+                return visitor.visit(element);
+            }
+        });
+    }
+}
